22898bb072f6037825f8e0d89541e19e49961bcc
fazzer195 <faizlucent12345.gmail.com>
2026-04-24 12:31:11 +0530

    sqllite db migartion upgrade

--- transportsoftware/settings.py
+++ transportsoftware/settings.py
@@ -85,39 +85,37 @@ WSGI_APPLICATION = 'transportsoftware.wsgi.application'
 # }
 IS_RENDER = os.environ.get('RENDER', False)
 
-if IS_RENDER:
-    # Render pe SQLite use karo
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    # Local development mein MySQL (optional)
-    
-
-   DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'transport_software',
-        'USER': 'root',          # XAMPP mein default 'root'
-        'PASSWORD': '',          # XAMPP mein default '' (blank)
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-          },
-      }
-    }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+# if IS_RENDER:
+   
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
 #     }
-# }
+# else:
+   
+#    DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'transport_software',
+#         'USER': 'root',          
+#         'PASSWORD': '',        
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#           },
+#       }
+#     }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
